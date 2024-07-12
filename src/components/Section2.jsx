e8dd9079df0633ffdaadfdbdab0bbabac00cb1ff
Mahak Vaid <mahakvaid1999@gmail.com>
2024-07-12 10:33:47 +0530

    commited

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -1,66 +1,72 @@
-import React, { useEffect, useState } from 'react'
-import { useStaticQuery,graphql } from 'gatsby'
-import CarousalItem from './CarousalItem';
+import React, { useEffect, useState } from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import CarousalItem from "./CarousalItem";
 
 const Section2 = () => {
-const [indexes,setIndexes] = useState([0,1])
-   useEffect(() => {
-         const interval = window.setInterval(() => {
-               if(indexes[0]===0) {
-                setIndexes([2,3])
-               }
-               else {
-                setIndexes([0,1])
-               }
-         },4000)  ;
+  const [indexes, setIndexes] = useState([0, 1]);
+  useEffect(() => {
+    const interval = window.setInterval(() => {
+      if (indexes[0] === 0) {
+        setIndexes([2, 3]);
+      } else {
+        setIndexes([0, 1]);
+      }
+    }, 4000);
 
-         return () => clearInterval(interval);
-         
-   },[indexes])
+    return () => clearInterval(interval);
+  }, []);
 
-    const data = useStaticQuery(graphql`
-       query CarousalQuery {
-  allFile(filter: {sourceInstanceName: {eq: "carousalText"}}, sort: {name: ASC}) {
-    nodes {
-      childMarkdownRemark {
-        frontmatter {
-          image {
-            childImageSharp {
-              gatsbyImageData
+  const data = useStaticQuery(graphql`
+    query CarousalQuery {
+      allFile(
+        filter: { sourceInstanceName: { eq: "carousalText" } }
+        sort: { name: ASC }
+      ) {
+        nodes {
+          childMarkdownRemark {
+            frontmatter {
+              image {
+                childImageSharp {
+                  gatsbyImageData
+                }
+              }
+              heading
+              text
             }
           }
-          heading
-          text
         }
       }
     }
-  }
-}
-            `);
-            
-   
-  return (
-    <div className='w-full h-[200px] flex items-center bg-[#F4F9FF] '>
-         <div className='w-full   mx-auto md:justify-between xl:gap-[100px] gap-[20px] px-12 max-w-[1200px] md:flex hidden carousal'>
-              {data?.allFile?.nodes.map((item,index) => {
-                console.log(item)
-                return(
-                  <CarousalItem h={item?.childMarkdownRemark?.frontmatter.heading} p={item?.childMarkdownRemark?.frontmatter.text} img={item?.childMarkdownRemark?.frontmatter.image}/>
-                )
-              })}
-             
-         </div>
-         <div className='w-full   mx-auto  gap-8 px-12  md:hidden flex justify-center'>
-              {data?.allFile?.nodes.map((item,index) => {
-                console.log(item)
-                return(
-                  indexes.includes(index) &&  <CarousalItem h={item?.childMarkdownRemark?.frontmatter.heading} p={item?.childMarkdownRemark?.frontmatter.text} img={item?.childMarkdownRemark?.frontmatter.image}/> 
-                )
-              })}
-             
-         </div>
-    </div>
-  )
-}
+  `);
 
-export default Section2
+  return (
+    <div className="w-full h-[200px] flex items-center bg-[#F4F9FF] ">
+      <div className="w-full   mx-auto md:justify-between xl:gap-[100px] gap-[20px] px-12 max-w-[1200px] md:flex hidden carousal">
+        {data?.allFile?.nodes.map((item, index) => {
+          return (
+            <CarousalItem
+              h={item?.childMarkdownRemark?.frontmatter.heading}
+              p={item?.childMarkdownRemark?.frontmatter.text}
+              img={item?.childMarkdownRemark?.frontmatter.image}
+            />
+          );
+        })}
+      </div>
+      <div className="w-full   mx-auto  gap-8 px-12  md:hidden flex justify-center">
+        {data?.allFile?.nodes.map((item, index) => {
+          return (
+            indexes.includes(index) && (
+              <CarousalItem
+                h={item?.childMarkdownRemark?.frontmatter.heading}
+                p={item?.childMarkdownRemark?.frontmatter.text}
+                img={item?.childMarkdownRemark?.frontmatter.image}
+              />
+            )
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Section2;
--- src/components/Section5.jsx
+++ src/components/Section5.jsx
@@ -1,67 +1,92 @@
-import React, { useState } from 'react'
-import { useStaticQuery,graphql } from 'gatsby'
-import LightHouse from './LightboxSection';
-import { GatsbyImage, getImage, StaticImage } from 'gatsby-plugin-image';
+import React, { useState, useEffect } from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import LightHouse from "./LightboxSection";
+import { GatsbyImage, getImage, StaticImage } from "gatsby-plugin-image";
 const Section5 = () => {
-    const data = useStaticQuery(graphql`
-        query lightboxQuery {
-   allFile(filter: {sourceInstanceName: {eq: "lightbox"}}) {
-     nodes {
-       childMarkdownRemark {
-         frontmatter {
-           image {
-             childImageSharp {
-               gatsbyImageData
-             }
-           }
-           
-         }
-       }
-     }
-   }
- }
-             `);
- 
-    
-    const [mainImg,setMainImg] = useState(getImage(data?.allFile?.nodes[0].childMarkdownRemark.frontmatter.image));
-   const [open,setOpen] = useState(false)
-    function handleImageChange(img) {
+  const data = useStaticQuery(graphql`
+    query lightboxQuery {
+      allFile(filter: { sourceInstanceName: { eq: "lightbox" } }) {
+        nodes {
+          childMarkdownRemark {
+            frontmatter {
+              image {
+                childImageSharp {
+                  gatsbyImageData
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const [mainImg, setMainImg] = useState(
+    getImage(data?.allFile?.nodes[0].childMarkdownRemark.frontmatter.image)
+  );
+  const [images, setImages] = useState([]);
+  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    images.length === 0 &&
+      data?.allFile?.nodes?.slice(1).map((node, index) => {
+        setImages((prev) => [
+          ...prev,
+          node.childMarkdownRemark.frontmatter.image,
+        ]);
+      });
+  }, []);
+  function handleImageChange(img) {
+    console.log("ran inside handle ");
     //    const newImgs = images.filter((image,index) => image!==img);
     //    setImages([...newImgs,mainImg]);
-    console.log("RAN")
-       setMainImg(getImage(img));
-
-    }
+    setMainImg(getImage(img));
+  }
 
   return (
     <>
-     <div className="w-[90%] 2xl:w-[70%] w-[97%] mx-auto h-fit flex flex-col mt-12 ">
+      <div className="w-[90%] 2xl:w-[70%] mx-auto h-fit flex flex-col mt-12 ">
         <div className="w-full md:h-[600px] h-[300px] relative">
-             <GatsbyImage image={mainImg} height={900} width={900} alt="img" className="w-full md:h-[600px] h-[300px] bg-cover max-h-[600px] object-cover rounded-xl rounded-md cursor-pointer" />  
-             <StaticImage src='../images/arrow.png'  className='absolute z-[1] w-[60px] h-[60px] top-[50%] right-[10px]'/>
-             <StaticImage src='../images/arrow.png' className='absolute rotate-180 z-[1] w-[60px] h-[60px] top-[50%] left-[10px]'/>
-             {/* <div className="m-2 absolute left-[10px] top-[50%] bg-white rounded-ful w-[40px] h-[40px] flex items-center justify-center rounded-full" onClick={() => handleImageChange(images[0])}>
-             <Image src={"/left-arrow.svg"} width={15} height={20} alt="arrow" className="cursor-pointer " />
-             </div> */}
-             {/* <div className="m-2 absolute right-[10px] top-[50%] bg-white rounded-ful w-[40px] h-[40px] flex items-center justify-center rounded-full" onClick={() => handleImageChange(images[3])}>
-             <Image src={"/right-arrow.svg"} width={15} height={15} alt="arrow" className="cursor-pointer"/>     
-             </div> */}
+          <GatsbyImage
+            image={mainImg}
+            height={900}
+            width={900}
+            alt="img"
+            className="w-full md:h-[600px] h-[300px] bg-cover max-h-[600px] object-cover rounded-xl cursor-pointer"
+            onClick={() => console.log("ran")}
+          />
+          <StaticImage
+            src="../images/arrow.png"
+            className="absolute z-[1] w-[60px] h-[60px] top-[50%] right-[10px]"
+            onClick={() => console.log("IMAGE")}
+          />
+          <StaticImage
+            src="../images/arrow.png"
+            className="absolute rotate-180 z-[1] w-[60px] h-[60px] top-[50%] left-[10px]"
+          />
         </div>
         <div className="flex gap-2 w-full max-w-full justify-between mt-2">
-            {data?.allFile?.nodes?.slice(1).map((img,index) =>
-                {
-                     const image = getImage(img.childMarkdownRemark.frontmatter.image)
-                   return <GatsbyImage image={image} width={900} height={900} alt="img" className="w-[24%] sm:rounded-2xl rounded-md bg-cover cursor-pointer"  key={index}/>
-                }
-               )}
-
+          {images.map((img, index) => {
+            const image = getImage(img);
+            return (
+              <div
+                onClick={() => handleImageChange(img)}
+                className="w-[24%] sm:rounded-2xl rounded-md bg-cover cursor-pointer"
+              >
+                <GatsbyImage
+                  image={image}
+                  width={900}
+                  height={900}
+                  alt="img"
+                  key={index}
+                />
+              </div>
+            );
+          })}
         </div>
-
-    </div>
-   {open && <LightHouse setOpen={setOpen} img={mainImg} open={open}/>}
+      </div>
+      {open && <LightHouse setOpen={setOpen} img={mainImg} open={open} />}
     </>
-  
-  )
-}
+  );
+};
 
-export default Section5
+export default Section5;
